660edc646f499cf3f8d0ce0391b2f07561828058
wugang <1067011755@qq.com>
2017-09-12 10:17:02 +0800

    修改图片选择

--- library/src/main/java/com/wugang/jsbridge/library/utils/ImagePickerPluginUtils.java
+++ library/src/main/java/com/wugang/jsbridge/library/utils/ImagePickerPluginUtils.java
@@ -81,7 +81,7 @@ public class ImagePickerPluginUtils {
                 NativeUtil.compressBitmap(bitmap, savePath);
                 fis = new FileInputStream(savePath);
                 String image = Base64.encodeToString(FileUtils.stream2Byte(fis), Base64.DEFAULT);
-                images.add(image);
+                images.add(imageItem.path+"$$"+image);
               }
               subscriber.onNext(images);
             }
